7b916d7074b6bd809eb261e18e5d2a37d3925d1e
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-30 19:19:32 +0200

    move stra_iteri to handle_input and remove parse_utils

--- src/parser/handle_input.c
+++ src/parser/handle_input.c
@@ -38,3 +38,21 @@ char	**get_file(char	*name)
 	free(file);
 	return (res);
 }
+
+char	*stra_iteri(char **arr, char *set, int id)
+{
+	size_t			j;
+	static size_t	i[5] = {0, 0, 0, 0, 0};
+
+	j = 0;
+	while (arr[i[id]] != NULL)
+	{
+		while (arr[i[id]][j] == 32 || (arr[i[id]][j] > 9 && arr[i[id]][j] < 13))
+			j++;
+		if (!ft_strncmp(arr[i[id]] + j, set, ft_strlen(set)))
+			return (arr[(i[id])++]);
+		j = 0;
+		(i[id])++;
+	}
+	return (NULL);
+}
