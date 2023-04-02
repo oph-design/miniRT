99cabf393d53f7874d6e4c76bf5a0ebde037c17d
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-02 18:31:38 +0200

    fix parse function

--- src/parser/parse_obj.c
+++ src/parser/parse_obj.c
@@ -7,7 +7,7 @@ t_object	parse_sphere(char *str, t_errors *ecode)
 
 	args = ft_split(str, '\t');
 	if (ft_stra_len(args) != 4)
-		return (*ecode = 1, new_sphere(new_vec(0, 0, 0), 0, new_vec(0, 0, 0)));
+		return (*ecode = 2, new_sphere(new_vec(0, 0, 0), 0, new_vec(0, 0, 0)));
 	sphere = new_sphere(get_vector(args[1], ecode, 1),
 			get_ratio(args[2], ecode), get_color(args[3], ecode));
 	return (sphere);
@@ -21,7 +21,7 @@ t_object	parse_cylinder(char *str, t_errors *ecode)
 
 	args = ft_split(str, '\t');
 	if (ft_stra_len(args) != 6)
-		return (*ecode = 1, new_cylinder(new_vec(0, 0, 0), new_vec(0, 0, 0),
+		return (*ecode = 2, new_cylinder(new_vec(0, 0, 0), new_vec(0, 0, 0),
 				size, new_vec(0, 0, 0)));
 	size[0] = get_ratio(args[3], ecode);
 	size[1] = get_ratio(args[4], ecode);
@@ -37,7 +37,7 @@ t_object	parse_plane(char *str, t_errors *ecode)
 
 	args = ft_split(str, '\t');
 	if (ft_stra_len(args) != 4)
-		return (*ecode = 1, new_plane(new_vec(0, 0, 0),
+		return (*ecode = 2, new_plane(new_vec(0, 0, 0),
 				new_vec(0, 0, 0), new_vec(0, 0, 0)));
 	plane = new_plane(get_vector(args[1], ecode, 1),
 			get_vector(args[2], ecode, 0), get_color(args[3], ecode));
