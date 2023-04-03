816fc6130793802ced3673831a9fccb28a25e638
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-03 10:46:35 +0200

    add closing of file after reading input

--- src/parser/getter.c
+++ src/parser/getter.c
@@ -15,20 +15,20 @@ double	get_ratio(char *str, t_errors *exit_code)
 
 t_vector	get_color(char *str, t_errors *exit_code)
 {
-	char			**split;
+	char			**args;
 	unsigned int	r;
 	unsigned int	g;
 	unsigned int	b;
 
-	split = ft_split(str, ',');
-	if (ft_stra_len(split) != 3)
-		return (ft_free_stra(split), *exit_code = VAL_NUM, new_vec(0, 0, 0));
-	if (is_number(split[0]) || is_number(split[1]) || is_number(split[2]))
+	args = ft_split(str, ',');
+	if (ft_stra_len(args) != 3)
+		return (ft_free_stra(args), *exit_code = VAL_NUM, new_vec(0, 0, 0));
+	if (is_number(args[0]) || is_number(args[1]) || is_number(args[2]))
 		*exit_code = NO_NUMBER;
-	r = ft_atoi(split[0]);
-	g = ft_atoi(split[1]);
-	b = ft_atoi(split[2]);
-	ft_free_stra(split);
+	r = ft_atoi(args[0]);
+	g = ft_atoi(args[1]);
+	b = ft_atoi(args[2]);
+	ft_free_stra(args);
 	if (r > 255 || g > 255 || b > 255)
 		return (*exit_code = VAL_RANGE, new_vec(0, 0, 0));
 	return (new_vec((double)r, (double)g, (double)b));
@@ -36,18 +36,18 @@ t_vector	get_color(char *str, t_errors *exit_code)
 
 t_vector	get_vector(char *str, t_errors *exit_code, int pos)
 {
-	char	**split;
+	char	**args;
 	double	x;
 	double	y;
 	double	z;
 
-	split = ft_split(str, ',');
-	if (ft_stra_len(split) != 3)
-		return (ft_free_stra(split), *exit_code = VAL_NUM, new_vec(0, 0, 0));
-	x = get_ratio(split[0], exit_code);
-	y = get_ratio(split[1], exit_code);
-	z = get_ratio(split[2], exit_code);
-	ft_free_stra(split);
+	args = ft_split(str, ',');
+	if (ft_stra_len(args) != 3)
+		return (ft_free_stra(args), *exit_code = VAL_NUM, new_vec(0, 0, 0));
+	x = get_ratio(args[0], exit_code);
+	y = get_ratio(args[1], exit_code);
+	z = get_ratio(args[2], exit_code);
+	ft_free_stra(args);
 	if (!pos)
 		if (x > 1.0 || x < -1.0 || y > 1.0 || y < -1.0 || z > 1.0 || z < -1.0)
 			return (*exit_code = VAL_RANGE, new_vec(0, 0, 0));
