1cc39a11ace3220783ff516d26e2b9afdd3a2fee
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-12 15:51:01 +0200

    implement find_invalid_identifier

--- src/parser/map.c
+++ src/parser/map.c
@@ -1,6 +1,6 @@
 #include "parser.h"
 
-void	parse_error(t_errors code, char *component, t_map **map, char **file);
+void	parse_error(t_errors code, char *component, t_map *map, char **file);
 
 t_map	*get_map(char **file)
 {
@@ -8,18 +8,20 @@ t_map	*get_map(char **file)
 	t_errors	exit_code;
 
 	if (file == NULL)
-		return (ft_putendl_fd("Error: file: empty file", 2), NULL);
+		parse_error(EMPTY, "file: ", NULL, file);
+	if (!find_invalid_ids(file))
+		parse_error(IDENTIFIER, "file: ", NULL, file);
 	exit_code = SUCCESS;
 	map = malloc(sizeof(t_map));
 	map->lighting = NULL;
 	map->camera = NULL;
 	map->objects = NULL;
 	exit_code = set_lighting(file, map);
-	parse_error(exit_code, "lighting: ", &map, file);
+	parse_error(exit_code, "lighting: ", map, file);
 	exit_code = set_camera(file, map);
-	parse_error(exit_code, "camera: ", &map, file);
+	parse_error(exit_code, "camera: ", map, file);
 	exit_code = get_obj_arr(file, map);
-	parse_error(exit_code, "objects: ", &map, file);
+	parse_error(exit_code, "objects: ", map, file);
 	return (ft_free_stra(file), map);
 }
 
@@ -33,14 +35,16 @@ void	free_map(t_map *map)
 	free(map);
 }
 
-void	parse_error(t_errors code, char *component, t_map **map, char **file)
+void	parse_error(t_errors code, char *component, t_map *map, char **file)
 {
 	if (code == SUCCESS)
 		return ;
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(component, 2);
-	if (code == FATAL)
-		ft_putendl_fd("fatal", 2);
+	if (code == EMPTY)
+		ft_putendl_fd("empty file", 2);
+	if (code == IDENTIFIER)
+		ft_putendl_fd("invalid Identifier", 2);
 	if (code == ARG_NUM)
 		ft_putendl_fd("wrong number of arguments", 2);
 	if (code == VAL_RANGE)
@@ -53,8 +57,7 @@ void	parse_error(t_errors code, char *component, t_map **map, char **file)
 		ft_putendl_fd("duplicate occurance of entity", 2);
 	if (code == NOT_FOUND)
 		ft_putendl_fd("non existent", 2);
-	free_map(*map);
+	free_map(map);
 	ft_free_stra(file);
-	*map = NULL;
 	exit(EXIT_FAILURE);
 }
