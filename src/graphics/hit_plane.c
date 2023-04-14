baefcf8705024d71cda7b923ddf99bf677cccb3e
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-14 14:17:34 +0200

    fix planes beeing visible if on camera height

--- src/graphics/hit_plane.c
+++ src/graphics/hit_plane.c
@@ -9,7 +9,7 @@ int	hit_plane(t_object pl, t_ray ray, int *pos, double *t)
 	if (div == 0)
 		return (0);
 	tt = dot(sub_vec(pl.pos, ray.origin), pl.orientation) / div;
-	if (tt < 0)
+	if (tt <= 0)
 		return (0);
 	else if (*t > tt)
 	{
