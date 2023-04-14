154aecb159b4d7c1370ae1765e4ad79db7950f7f
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-14 13:59:32 +0200

    implement hit_plane

---
+++ src/graphics/hit_plane.c
@@ -0,0 +1,21 @@
+#include "minirt.h"
+
+int	hit_plane(t_object pl, t_ray ray, int *pos, double *t)
+{
+	double		div;
+	double		tt;
+
+	div = dot(ray.direction, pl.orientation);
+	if (div == 0)
+		return (0);
+	tt = dot(sub_vec(pl.pos, ray.origin), pl.orientation) / div;
+	if (tt < 0)
+		return (0);
+	else if (*t > tt)
+	{
+		*t = tt;
+		pos[INDEX_HIT] = pos[INDEX];
+		return (1);
+	}
+	return (0);
+}
