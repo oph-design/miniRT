7e3dfc71e9259721ad54dd549ce7f99998060015
LaurinUB <Laurinub@yahoo.de>
2023-04-15 13:01:36 +0200

    adding more realistic plane coloring and restructuring code

--- src/graphics/cast_light.c
+++ src/graphics/cast_light.c
@@ -8,22 +8,34 @@ t_vector	color_to_ratio(t_vector color)
 	return (color);
 }
 
+t_vector	get_object_normal(t_object obj, t_vector hit)
+{
+	if (obj.type == PLANE)
+	{
+		obj.pos.x *= -1;
+		obj.pos.y *= -1;
+		obj.pos.z *= -1;
+		return (normalize(obj.pos));
+	}
+	else if (obj.type == SPHERE)
+		return (normalize(sub_vec(hit, obj.pos)));
+	return (new_vec(0, 0, 0));
+}
+
 t_vector	cast_light(t_map *map, int pos, t_vector hit)
 {
 	t_vector	ambient;
 	t_vector	light;
-	t_vector	dir;
 	t_vector	light_dir;
 	double		radiant;
 
 	ambient = mult_double_vec(map->lighting->a_ratio, map->lighting->a_color);
 	ambient = color_to_ratio(ambient);
+	ambient = mult_vec(ambient, map->objects[pos].color);
 	light = mult_double_vec(map->lighting->l_ratio, map->lighting->l_color);
 	light = color_to_ratio(light);
 	light_dir = normalize(sub_vec(map->lighting->pos, hit));
-	dir = normalize(sub_vec(hit, map->objects[pos].pos));
-	radiant = dot(light_dir, dir);
-	ambient = mult_vec(ambient, map->objects[pos].color);
+	radiant = dot(light_dir, get_object_normal(map->objects[pos], hit));
 	light = mult_vec(light,
 			mult_double_vec(radiant, map->objects[pos].color));
 	return (add_clamp(light, ambient, 0, 255));
--- src/graphics/cast_shadow.c
+++ src/graphics/cast_shadow.c
@@ -1,30 +1,29 @@
 #include "minirt.h"
 
-static void	intercept_loop(t_map *map, t_vector hit, int *i, double *t)
+static void	intercept_loop(t_map *map, t_vector hit, int i, double *t)
 {
-	if (map->objects[i[INDEX]].type == SPHERE)
+	if (map->objects[i].type == SPHERE)
 	{
-		hit_sphere(map->objects[i[INDEX]],
+		hit_sphere(map->objects[i],
 			new_ray(hit, sub_vec(map->lighting->pos, hit)), NULL, &t[0]);
-		hit_sphere(map->objects[i[INDEX]],
+		hit_sphere(map->objects[i],
 			new_ray(hit, sub_vec(hit, map->lighting->pos)), NULL, &t[1]);
 	}
 }
 
 int	is_shaded(t_map *map, int pos, t_vector hit)
 {
-	int		i[2];
+	int		i;
 	double	t[2];
 
-	i[INDEX] = 0;
-	i[INDEX_HIT] = pos;
+	i = 0;
 	t[0] = INFINITY;
 	t[1] = INFINITY;
-	while ((int)map->obj_count > i[INDEX])
+	while ((int)map->obj_count > i)
 	{
-		if (i[INDEX] != pos)
+		if (i != pos)
 			intercept_loop(map, hit, i, t);
-		i[INDEX]++;
+		i++;
 	}
 	if (t[0] > t[1] && t[0] < INFINITY)
 		return (1);
