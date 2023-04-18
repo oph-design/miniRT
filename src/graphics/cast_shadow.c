eaf435a4c500e3237cd8a9874950eb8dcb6e73ef
LaurinUB <Laurinub@yahoo.de>
2023-04-18 19:24:12 +0200

    objects behind light source no longer shadowing objects infront

--- src/graphics/cast_shadow.c
+++ src/graphics/cast_shadow.c
@@ -1,17 +1,33 @@
 #include "minirt.h"
 
+int	is_in_front(t_vector light_dir, t_ray light_ray, double t)
+{
+	double	source_length;
+	double	hit_length;
+
+	source_length = vec_length_squared(light_dir);
+	hit_length = vec_length_squared(sub_vec(light_ray.origin,
+				at(light_ray, t)));
+	if (source_length > hit_length)
+		return (1);
+	return (0);
+}
+
 static int	intercept_obj(t_map *map, t_hit hit, size_t i)
 {
 	t_vector	light_dir;
+	t_ray		light_ray;
 	double		t;
 
 	t = INFINITY;
-	light_dir = normalize(sub_vec(map->lighting->pos, hit.hitpoint));
+	light_dir = sub_vec(map->lighting->pos, hit.hitpoint);
+	light_ray = new_ray(hit.hitpoint, light_dir);
 	if (map->objects[i].type == SPHERE)
 		hit_sphere(map->objects[i],
 			new_ray(add_vec(mult_double_vec(ZERO, hit.normal),
 					hit.hitpoint), light_dir), NULL, &t);
-	if (t < INFINITY && t + ZERO > ZERO)
+	if (t < INFINITY && t + ZERO > ZERO
+		&& is_in_front(light_dir, light_ray, t))
 		return (1);
 	return (0);
 }
