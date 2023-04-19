2a392b846b57a40c2e6cd96121b2b726a3afe473
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-19 11:41:57 +0200

    implement resize hook

--- src/graphics/window.c
+++ src/graphics/window.c
@@ -23,6 +23,7 @@ void	setup_window(t_map *map)
 		window_panic(map);
 	draw(map);
 	mlx_loop_hook(map->window->mlx, input, map);
+	mlx_resize_hook(map->window->mlx, resizing, map);
 	mlx_loop(map->window->mlx);
 	mlx_delete_image(map->window->mlx, map->window->image);
 	mlx_terminate(map->window->mlx);
