8ccc099e26cc82da47f0ec403c633ec7c27f60c7
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-23 18:46:50 +0200

    implement parsing for multiple light sources

--- src/objects/lighting.c
+++ src/objects/lighting.c
@@ -6,9 +6,13 @@ void	set_amblight(t_lighting *light, double ratio, t_vector color)
 	light->a_color = color;
 }
 
-void	set_light(t_lighting *light, double ratio, t_vector color, t_vector c)
+t_lighting	get_light(double ratio, t_vector color, t_vector c)
 {
-	light->l_ratio = ratio;
-	light->l_color = color;
-	light->pos = c;
+	t_lighting	light;
+
+	light.l_ratio = ratio;
+	light.l_color = color;
+	light.pos = c;
+	light.obj = new_light(light.pos, 0.5, light.l_color);
+	return(light);
 }
