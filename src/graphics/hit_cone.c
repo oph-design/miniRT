0b6eeafb0e3692565e05db8788638a6c5520b1a2
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-23 17:51:25 +0200

    somewhat maybe right implementation

--- src/graphics/hit_cone.c
+++ src/graphics/hit_cone.c
@@ -4,12 +4,19 @@ int	hit_cone(t_object cn, t_ray ray, size_t *pos, double *t)
 {
 	t_vector	vals;
 	t_vector	p;
+	t_vector	d_proj;
+	double		k;
 	double		tmp[3];
 
-	p = sub_vec(cn.pos, ray.origin);
-	vals.x = 0;
-	vals.y = 0;
-	vals.z = 0;
+	p = sub_vec(ray.origin, cn.pos);
+	d_proj = sub_vec(ray.direct, mult_double_vec(dot(cn.direct, ray.direct), cn.direct));
+	k = pow(atan(cn.radius / cn.height) / 2, 2);
+	vals.x = 	dot(d_proj, d_proj)
+				- (1 + k * pow(dot(ray.direct, cn.direct), 2));
+	vals.y = 	dot(d_proj, sub_vec(p, mult_double_vec(dot(p, cn.direct), cn.direct)))
+				- (1 + k * dot(ray.direct, cn.direct) * dot(sub_vec(p, cn.pos), cn.direct));
+	vals.z = 	dot(sub_vec(p, cn.pos), sub_vec(p, sub_vec(cn.pos, sub_double_vec(tan(atan(cn.radius / cn.height) / 2) * cn.height, cn.direct))))
+				- (1 + k * pow(dot(sub_vec(p, cn.pos), cn.direct), 2));
 	if (vals.y * vals.y - vals.x * vals.z < 0)
 		return (0);
 	tmp[0] = (-vals.y - sqrt(vals.y * vals.y - vals.x * vals.z) / (vals.x));
@@ -24,3 +31,10 @@ int	hit_cone(t_object cn, t_ray ray, size_t *pos, double *t)
 		pos[INDEX_HIT] = pos[INDEX];
 	return (1);
 }
+
+// q = p - c
+// d_proj = d - dot(d, a) * a
+// k = tan(theta/2) * h
+// A = dot(d_proj, d_proj) - (1 + tan(theta/2) * tan(theta/2)) * dot(d, a) * dot(d, a)
+// B = 2 * (dot(d_proj, q - dot(q, a) * a) - (1 + tan(theta/2) * tan(theta/2)) * dot(d, a) * dot(q - c, a))
+// C = dot(q - c, q - c - k * a) - (1 + tan(theta/2) * tan(theta/2)) * dot(q - c, a) * dot(q - c, a)
