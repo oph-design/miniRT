e3a8c9c433c1004c7502b1b901988d83fc99a356
oph-design <ole.paul.heinzelmann@gmail.com>
2023-04-24 11:17:09 +0200

    still not working

--- src/graphics/hit_cone.c
+++ src/graphics/hit_cone.c
@@ -3,20 +3,17 @@
 int	hit_cone(t_object cn, t_ray ray, size_t *pos, double *t)
 {
 	t_vector	vals;
-	t_vector	p;
-	t_vector	d_proj;
-	double		k;
+	t_vector	q;
+	t_vector	d;
+	double		a;
 	double		tmp[3];
 
-	p = sub_vec(ray.origin, cn.pos);
-	d_proj = sub_vec(ray.direct, mult_double_vec(dot(cn.direct, ray.direct), cn.direct));
-	k = pow(atan(cn.radius / cn.height) / 2, 2);
-	vals.x = 	dot(d_proj, d_proj)
-				- (1 + k * pow(dot(ray.direct, cn.direct), 2));
-	vals.y = 	dot(d_proj, sub_vec(p, mult_double_vec(dot(p, cn.direct), cn.direct)))
-				- (1 + k * dot(ray.direct, cn.direct) * dot(sub_vec(p, cn.pos), cn.direct));
-	vals.z = 	dot(sub_vec(p, cn.pos), sub_vec(p, sub_vec(cn.pos, sub_double_vec(tan(atan(cn.radius / cn.height) / 2) * cn.height, cn.direct))))
-				- (1 + k * pow(dot(sub_vec(p, cn.pos), cn.direct), 2));
+	d = normalize(ray.direct);
+	q = sub_vec(ray.origin, cn.pos);
+	a = pow(cn.radius / cn.height, 2);
+	vals.x = dot(d, ray.direct) - pow(dot(cn.direct, d) / cn.height, 2) * a;
+	vals.y = dot(q, d) - dot(q, cn.direct) * a;
+	vals.z = dot(q, q) - pow(dot(q, cn.direct) / cn.height, 2) * a;
 	if (vals.y * vals.y - vals.x * vals.z < 0)
 		return (0);
 	tmp[0] = (-vals.y - sqrt(vals.y * vals.y - vals.x * vals.z) / (vals.x));
@@ -32,9 +29,15 @@ int	hit_cone(t_object cn, t_ray ray, size_t *pos, double *t)
 	return (1);
 }
 
-// q = p - c
-// d_proj = d - dot(d, a) * a
-// k = tan(theta/2) * h
-// A = dot(d_proj, d_proj) - (1 + tan(theta/2) * tan(theta/2)) * dot(d, a) * dot(d, a)
-// B = 2 * (dot(d_proj, q - dot(q, a) * a) - (1 + tan(theta/2) * tan(theta/2)) * dot(d, a) * dot(q - c, a))
-// C = dot(q - c, q - c - k * a) - (1 + tan(theta/2) * tan(theta/2)) * dot(q - c, a) * dot(q - c, a)
+// a = atan(r / h)
+// q = o - p
+// x = p_x + t * v_x
+// y = p_y + t * v_y
+// z = p_z + t * v_z
+// (x - p_x)^2 + (y - p_y)^2 = (z - p_z)^2 * (r / h)^2
+// Substitute the parametric equation of the ray into the equation of the cone:
+
+// b = 2 * (q . d) - 2 * (q . v) * (r / h)^2
+// c = (q . q) - ((q . v) / h)^2 * r^2
+// a = (d . d) - ((v . d) / h)^2 * r^2
+// t1, t2 = (-b +/- sqrt(b^2 - 4ac)) / 2a
